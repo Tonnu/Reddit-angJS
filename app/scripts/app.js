9f3fe7247a48b022e5c48bd3999b32904cfd1f67
Toon Verbeek <toon.verbeek1@gmail.com>
2014-02-20 21:17:47 +0100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+/* global app:true */
+
+var app = angular.module('angNewsApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/posts.js
@@ -0,0 +1,23 @@
+'use strict';
+
+app.controller('PostsCtrl', function ($scope, Post) {
+    $scope.post = {url: 'http://', title: ''};
+    $scope.posts = Post.get();
+
+    $scope.submitPost = function () {
+        Post.save($scope.post, function (ref) {
+            $scope.posts[ref.name] = $scope.post;
+            $scope.post = {url: 'http://', title: ''};
+        });
+        $scope.post = {url: 'http://', title: ''};
+    };
+
+    
+    $scope.deletePost = function (postId) {
+        Post.delete({id: postId}, function () {
+            delete $scope.posts[postId];
+        });
+    };
+
+
+});
